48df1f80862a5de88779ffe474ccaf3a860dcf4c
Peter Pouliot <peter@pouliot.net>
2018-10-02 17:29:31 +0000

    updating

--- cloud-config.yml/rancheros-1.4.1-amd64.pxe_installer.sh
+++ cloud-config.yml/rancheros-1.4.1-amd64.pxe_installer.sh
@@ -16,10 +16,10 @@ sudo hostnamectl set-hostname $NEWHOSTNAME
 # Download cloud-config
 
 
-wget http://i.pxe.to/cloud-config.yml/rancheros-1.4.0-amd64.cloud-config.yml
-sed -i "/hostname: rancheros/c\hostname: $NEWHOSTNAME" ./rancheros-1.4.0-amd64.cloud-config.yml
+wget http://i.pxe.to/cloud-config.yml/rancheros-1.4.1-amd64.cloud-config.yml
+sed -i "/hostname: rancheros/c\hostname: $NEWHOSTNAME" ./rancheros-1.4.1-amd64.cloud-config.yml
 
-#sed -i "/ discovery: \"https://discovery.etcd.io/<token>\"/c\ $DISCOVERY" ./rancheros-1.4.0-amd64.cloud-config.yml
+#sed -i "/ discovery: \"https://discovery.etcd.io/<token>\"/c\ $DISCOVERY" ./rancheros-1.4.1-amd64.cloud-config.yml
 
 
 # Run install 
@@ -30,5 +30,5 @@ INSTALL_DISK="/dev/vda"
 if ! fdisk -l $INSTALL_DISK; then
 	INSTALL_DISK="/dev/sda"
 fi
-sudo ros install -c ./rancheros-1.4.0-amd64.cloud-config.yml -d $INSTALL_DISK -f --append "console=ttyS0,115200n9 serial"
+sudo ros install -c ./rancheros-1.4.1-amd64.cloud-config.yml -d $INSTALL_DISK -f --append "console=ttyS0,115200n9 serial"
 
